b5da22c44233beea74008e2751fd72d4dd6cb226
MarianoFunes <mgfg83@gmail.com>
2023-03-05 22:43:21 -0300

    footer

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -26,34 +26,7 @@ function Chart (){
         <div className="card shadow mb-4">
             <div className="card-body">
                 <div className="table-responsive">
-                    <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
-                        <thead>
-                            <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Género</th>
-                                <th>Premios</th>
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Título</th>
-                                <th>Duración</th>
-                                <th>Rating</th>
-                                <th>Género</th>
-                                <th>Premios</th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-                            {
-                            tableRowsData.map( ( row , i) => {
-                                return <ChartRow { ...row} key={i}/>
-                            })
-                            }
-
-                        </tbody>
-                    </table>
+                   
                 </div>
             </div>
         </div>
